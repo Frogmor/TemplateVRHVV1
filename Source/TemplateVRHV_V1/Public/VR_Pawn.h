1e2f618b114e4ed178127aa73bfd7b98ab444325
DESKTOP-SLQ2BMS\Valentin <haehnelval@gmail.com>
2017-10-20 12:10:49 +0200

    Ajout de quelque chose

--- Source/TemplateVRHV_V1/Public/VR_Pawn.h
+++ Source/TemplateVRHV_V1/Public/VR_Pawn.h
@@ -1,9 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+/*Gnu general public license version 3
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 #pragma once
 
+#include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "VR_Pawn.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGravityProperty {
+
+	GENERATED_USTRUCT_BODY()
+		//respond to unevent terrain - gravaty Must also be enabled
+		UPROPERTY(EditAnywhere, Category = "VR")
+		bool respondToUneventTerrain = false;
+
+		//how fast this VR Pawn will fall with gravity
+		UPROPERTY(EditAnywhere, Category = "VR")
+		float gravityStrength = 3.f;
+
+		//how far should the check for a floor be
+		UPROPERTY(EditAnywhere, Category = "VR")
+		float floorTraceRange = 150.f;
+
+		//min Z offset befor terrain is considered uneven
+		UPROPERTY(EditAnywhere, Category = "VR")
+		float floorTraceTolerance = 3.f;
+
+		//direction where this VR Pawn will "fall"
+		UPROPERTY(EditAnywhere, Category = "VR")
+		FVector gravityDirection = FVector(0.f, 0.f, 0.f);
+};
+
 
 UCLASS()
 class TEMPLATEVRHV_V1_API AVR_Pawn : public APawn
